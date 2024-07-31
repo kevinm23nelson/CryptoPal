51244601e559a83f412977c1a3b7d7ed042a2d4c
Kevin Nelson <km.nelson@outlook.com>
2024-07-31 18:28:23 -0400

    Added further CSS styling to pages and headers along with refactors

--- CryptoPal/src/components/CurrencyDetailsPage/CurrencyDetailsPage.js
+++ CryptoPal/src/components/CurrencyDetailsPage/CurrencyDetailsPage.js
@@ -33,28 +33,34 @@ const CurrencyDetailsPage = () => {
     });
   };
 
+  const performanceClass = parseFloat(currency?.changePercent24Hr) >= 0 ? 'positive-performance' : 'negative-performance';
+
   return (
     <div className="currency-details-page">
       {errorMessage ? (
         <p className="error-message">{errorMessage}</p>
       ) : currency ? (
-        <div className="currency-details">
-          <p>Name: {currency.name}</p>
-          <p>Rank: {currency.rank}</p>
-          <p>Symbol: {currency.symbol}</p>
-          <p>Supply: {currency.supply}</p>
-          <p>Max Supply: {currency.maxSupply}</p>
-          <p>Market Cap USD: {formatPrice(currency.marketCapUsd)}</p>
-          <p>Volume USD (24Hr): {formatPrice(currency.volumeUsd24Hr)}</p>
-          <p>Price USD: {formatPrice(currency.priceUsd)}</p>
-          <p>Change (24hr): {parseFloat(currency.changePercent24Hr).toFixed(2)}%</p>
-          <p>VWAP (24Hr): {formatPrice(currency.vwap24Hr)}</p>
-          <button
-            className="return-button"
-            onClick={() => navigate(location.state?.from === 'dashboard' ? '/' : '/explore')}
-          >
-            Return to {location.state?.from === 'dashboard' ? 'Dashboard' : 'Explore'}
-          </button>
+        <div className="currency-view-details">
+          <div className={`currency-header ${performanceClass}`}>
+            <p>Coin: {currency.name}</p>
+            <p className="currency-symbol">Symbol: {currency.symbol}</p>
+          </div>
+          <div className="currency-details-body">
+            <p>Rank: {currency.rank}</p>
+            <p>Supply: {currency.supply}</p>
+            <p>Max Supply: {currency.maxSupply}</p>
+            <p>Market Cap USD: {formatPrice(currency.marketCapUsd)}</p>
+            <p>Volume USD (24Hr): {formatPrice(currency.volumeUsd24Hr)}</p>
+            <p>Price USD: {formatPrice(currency.priceUsd)}</p>
+            <p>Change (24hr): {parseFloat(currency.changePercent24Hr).toFixed(2)}%</p>
+            <p>VWAP (24Hr): {formatPrice(currency.vwap24Hr)}</p>
+            <button
+              className="return-button"
+              onClick={() => navigate(location.state?.from === 'dashboard' ? '/' : '/explore')}
+            >
+              Return to {location.state?.from === 'dashboard' ? 'Dashboard' : 'Explore'}
+            </button>
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
